ebc854f40ebf9b3cfda6593f7f80becf2fc39531
Сергей Чувычкин <chuvichkin@yandex.ru>
2022-09-09 21:29:07 +0300

    fix_subscribe

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import CustomUserViewSet
+
 app_name = 'users'
 
 router = DefaultRouter()
 
+router.register('users', CustomUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
